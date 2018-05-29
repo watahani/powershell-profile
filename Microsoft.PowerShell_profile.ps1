7130fafaa0a3dc8848bb855d21b8cc09b2b5f9f8
Wataru Haniyama <haniyama_wataru@sgk.jp>
2018-05-30 00:14:19 +0900

    FIX file encode to shift-js

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,8 @@
-# Windows で Linux 形式のパスを作る (docker run 時の -v オプションに渡すにはこの形式が必要)
+# Windows Path to Linux
 function pwd_as_linux {
     "/$((pwd).Drive.Name.ToLowerInvariant())/$((pwd).Path.Replace('\', '/').Substring(3))"
 }
-# File-Hash を取得する
+# Calculate File-Hashes
 function Get-File-Sha1Hashes {
     $files = Get-ChildItem -Force
     foreach ($file in $files) {
@@ -14,8 +14,33 @@ function Get-File-Sha1Hashes {
         }
     }
 }
+
+# Check Command Enable
+function Command-Enable ($command) {
+    Get-Command $command -ea SilentlyContinue | Out-Null
+        if ($? -eq $true) { 
+            return $true
+        }
+        return $false
+}
+
+# String To Base64
+function Encode-Base64($planeText){
+    $byte = ([System.Text.Encoding]::Default).GetBytes($planeText)
+    $b64enc = [Convert]::ToBase64String($byte)
+    return $b64enc
+}
+
+# Base64 To Plane Text 
+function Decode-Base64($base64){
+    $byte = [System.Convert]::FromBase64String($base64)
+    $txt = [System.Text.Encoding]::Default.GetString($byte)
+    return $txt
+}
+
+
 $ProfileFolder = Split-Path $profile
 Import-Module $ProfileFolder\bf783d2a5378f32dbacb40d8897e7942\profile.ps1
 Import-Module $ProfileFolder\3d4be89cdd501d815e8ab03268bbb41c\profile.ps1
 Import-Module $ProfileFolder\posh-git\src\posh-git.psd1
-try { $null = gcm pshazz -ea stop; pshazz init 'default' } catch { }
+try { $null = gcm pshazz -ea stop; pshazz init } catch { }
