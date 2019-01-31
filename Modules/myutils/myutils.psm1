b5e3f1c5d914a06b2d7587273d781319593408cf
Wataru Haniyama <haniyama.wataru+work@gmail.com>
2019-01-31 18:51:28 +0900

    fix non valid conditional sentence

--- Modules/myutils/myutils.psm1
+++ Modules/myutils/myutils.psm1
@@ -85,7 +85,7 @@ function Decode-Base64Url {
     )
     process {
         $missingCharacters = 4 - $base64Url.Length % 4
-        if($missingCharacters -lt 4)
+        if($missingCharacters -gt 0)
         {
             $missingString = New-Object System.String -ArgumentList @( '=', $missingCharacters )
             $base64Url = $base64Url + $missingString       
